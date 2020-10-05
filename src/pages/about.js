85edd266ef1722706031323b0005f38d2e361dd0
CWilson <chris@cwilson.us>
2020-10-05 19:38:02 -0400

    Additional typography practice

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,12 @@ export default function About() {
         <Link to="/">Home</Link>
         <Header headerText="About Gatsby" />
         <Header headerText="It's pretty cool." />
+        <div>
+      <h1>About me</h1>
+      <p>
+        I’m good enough, I’m smart enough, and gosh darn it, people like me! -Gatsby.js
+      </p>
+    </div>
       <p>Such wow. Very React.</p>
       <p>Following a Tutorial to Learn Gatsby.</p>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
     <div style={{ color: `purple`}}>
       <Link to="/contact/">Contact</Link>
       <p></p>
@@ -14,6 +15,12 @@ export default function Home() {
       <Header headerText="Hello Gatsby!" />
     <p>What a world!</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+    </div>
     </div>
   );
 }
